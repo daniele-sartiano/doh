b47246e0dc087dad3a832ac1bc99f258ad97d5a7
Daniele <daniele.sartiano@gmail.com>
2020-06-10 17:45:01 +0200

    Created class BinsGenerator

--- doh.py
+++ doh.py
@@ -1,63 +1,65 @@
 #!/usr/bin/env python3
 
 from scapy.all import *
-import os
 import argparse
 import sys
-
 
 load_layer("ip")
 load_layer("tls")
 
-def scale(len):
-    if(len > 1504):
-        len = 1504
-        
-    return(int(len/32))
+class BinsGenerator:
 
-maxlen = int(1504/32)
-chains = {}
-
-
-def newchain():
-    c = [0 for x in range(maxlen+1)]
-    return(c)
-
-def updatechain(who, slot):
-    if who not in chains:
-        chains[who] = newchain()
-
-    chains[who][slot] = chains[who][slot] + 1
-
+    MAXLEN = int(1504/32)
     
-def markov(src, dst, len):
-    slot = scale(len) 
-    updatechain(src, slot)
-    updatechain(dst, slot)
+    def __init__(self, pcap, filter):
+        self.chains = {}
+
+        pkts = sniff(offline=pcap, prn=self.pkt_parser, filter=filter)
+
+    @staticmethod
+    def scale(len):
+        if(len > 1504):
+            len = 1504
+        return(int(len/32))
         
+    def newchain(self):
+        c = [0 for x in range(self.MAXLEN+1)]
+        return(c)
 
-def pkt_parser(pkt):
-    if pkt.haslayer('TLS'):
-        ip = pkt.getlayer(IP)
-        tcp = pkt.getlayer(TCP)
-        tls = pkt.getlayer(TLS)
+    def updatechain(self, who, slot):
+        if who not in self.chains:
+            self.chains[who] = self.newchain()
+        self.chains[who][slot] = self.chains[who][slot] + 1
 
-        markov(ip.src+":"+str(tcp.sport), ip.dst+":"+str(tcp.dport), tls.len)
+    def markov(self, src, dst, len):
+        slot = self.scale(len) 
+        self.updatechain(src, slot)
+        self.updatechain(dst, slot)
 
-def printchain(a):
-    #print('-->', a)
-    s = sum(a)
-    for x in range(maxlen):
-        a[x] = int((a[x]*100)/s)
+    def pkt_parser(self, pkt):
+        if pkt.haslayer('TLS'):
+            ip = pkt.getlayer(IP)
+            tcp = pkt.getlayer(TCP)
+            tls = pkt.getlayer(TLS)
 
-    print(a)
-    
+            self.markov(ip.src+":"+str(tcp.sport), ip.dst+":"+str(tcp.dport), tls.len)
+
+    @staticmethod
+    def printchain(a):
+        #print('-->', a)
+        s = sum(a)
+        for x in range(BinsGenerator.MAXLEN):
+            a[x] = int((a[x]*100)/s)
+
+        print(a)
+
+        
 
 ##############################################################################################################
 # MAIN
 
 def main():
-    parser = argparse.ArgumentParser(description='DoH bins extractor')
+    parser = argparse.ArgumentParser(description='DoH bins generator')
     parser.add_argument('pcap', type=str, help='pcap file')
     parser.add_argument('dsn_ip_addresses', type=str, help='list of dns ip addresses separated by comma')
 
@@ -71,9 +73,9 @@ def main():
         if hosts:
             f += ' and ({})'.format(' or '.join(hosts))
 
-    pkts = sniff(offline=args.pcap, prn=pkt_parser, filter=f)
-    for host in chains:
-        printchain(chains[host])
+    generator = BinsGenerator(args.pcap, f)
+    for host in generator.chains:
+        BinsGenerator.printchain(generator.chains[host])
 
 if __name__ == '__main__':
     main()
